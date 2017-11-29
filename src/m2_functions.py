bac7834f394e2e4458afa8f63ebfcc7ed0539b0d
woodrc <woodrc@rose-hulman.edu>
2017-11-29 16:33:16 -0500

    Need to do TODO 5

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,12 +3,12 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Rachel Wood.
 """
 
 ########################################################################
 #
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE and...
+# DONE 1
 #
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
@@ -34,6 +34,7 @@ def main():
     window = rg.TurtleWindow()
 
     turtle1()
+    turtle4()
     turtle3()
     turtle2()
     turtle2()
@@ -103,9 +104,34 @@ def turtle3():
     maja.end_fill()
 
 
+def turtle4():
+    """
+    Constructs a default SimpleTurtle.
+    Makes that SimpleTurtle go forward 300 units
+    and then draw a black-filled circle.
+    """
+    blob = rg.SimpleTurtle('triangle')
+    blob.pen = rg.Pen('green', 7)
+
+    blob.pen_up()
+    blob.forward(100)
+    blob.pen_down()
+
+    blob.paint_bucket = rg.PaintBucket('violet')
+
+    blob.begin_fill()
+    blob.draw_circle(50)
+    blob.draw_circle(60)
+    blob.draw_circle(70)
+    blob.draw_circle(80)
+    blob.draw_circle(90)
+    blob.draw_circle(100)
+    blob.end_fill()
+
+
 ########################################################################
 #
-# TODO: 2.
+# DONE 2
 #   READ the code above.  Be sure you understand:
 #     -- How many functions are defined above?
 #           (Answer: 4)
@@ -123,10 +149,18 @@ def turtle3():
 #
 #   RELATE what is DRAWN to the CODE above.  Be sure you understand:
 #       -- WHEN does the code in   main   run?
+#           Line 225
 #       -- WHEN does the code in   turtle1   run?
+#            After line 225 it is on line 36
 #                    the code in   turtle2   run?
+#            After line 225 it is on line 38 and 39
 #                    the code in   turtle3   run?
+#            After line 225 it is on line 37
 #       -- For each of the above, WHY does that code run when it does?
+#            It finds a function, then its definition. main is defined before
+#           its executes. The respective function is called on line 36,
+# 37 38 and 39 as told bellow.When a fucion is called, the difinition is found
+#           and exicuted.
 #
 #     ** ASK QUESTIONS if you are uncertain. **
 #
@@ -137,7 +171,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 3.
+# DONE 3
 #   Define another function,
 #   immediately below the end of the definition of   turtle3   above.
 #   Name your new function   turtle4.
@@ -167,7 +201,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 4.
+# DONE 4
 #   Add a line to   main   that CALLS your new function immediately
 #   AFTER  main  calls turtle1.  So:
 #     -- the SimpleTurtle from turtle1 should move,
